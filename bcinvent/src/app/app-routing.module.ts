603259a0656462146d43fa3664e19c29c9361e05
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2022-10-06 10:39:35 +0200

    unused components removed

--- bcinvent/src/app/app-routing.module.ts
+++ bcinvent/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddClassComponent } from './add-class/add-class.component';
 import { ClassesComponent } from './classes/classes.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { KemptonClassesComponent } from './kempton-classes/kempton-classes.component';
 import { LoginComponent } from './login/login.component';
@@ -16,7 +15,6 @@ const routes: Routes = [
 
   { path: '', redirectTo: 'login',pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent},
   { path: 'home', component: HomeComponent},
 
   { path: 'classes', 
--- bcinvent/src/app/app.module.ts
+++ bcinvent/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -33,7 +32,6 @@ import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     LoginComponent,
     SidenavComponent,
     HomeComponent,
